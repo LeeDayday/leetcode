4cc4fae5888678d6565b5c2e1b2664f5739c3707
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2025-09-25 23:31:39 +0900

    Create 120.py

---
+++ pending/120.py
@@ -0,0 +1,10 @@
+# 120. Triangle
+# https://leetcode.com/problems/triangle/?envType=daily-question&envId=2025-09-25
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        for i in range(n - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
