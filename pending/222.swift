ffe470815150721e5d70a69f284a2c7c44929bf7
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2026-03-06 00:37:18 +0900

    Create 222.swift

---
+++ pending/222.swift
@@ -0,0 +1,44 @@
+// 222. Count Complete Tree Nodes
+// https://leetcode.com/problems/count-complete-tree-nodes/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func countDepth(_ node: TreeNode?) -> Int {
+        var node = node
+        var depth: Int = 0
+
+        while node != nil {
+            node = node?.left
+            depth += 1
+        }
+        return depth
+    }
+    func countNodes(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+
+        let leftDepth: Int = countDepth(root.left)
+        let rightDepth: Int = countDepth(root.right)
+
+        print(leftDepth, rightDepth)
+        if leftDepth == rightDepth {
+            return 1 << leftDepth + countNodes(root.right)
+        }
+        else {
+            return 1 << rightDepth + countNodes(root.left)
+        }
+    }
+}
