2052b1326509d16f27bacddb6e8b86b53d75e31a
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2026-05-16 17:38:26 +0900

    Create 242.py

---
+++ pending/242.py
@@ -0,0 +1,10 @@
+# 242. Valid Anagram
+# https://leetcode.com/problems/valid-anagram/description/
+
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        return counter_s == counter_t
