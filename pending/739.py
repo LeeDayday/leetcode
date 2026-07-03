497810f17f87c6c213ec8781744ccf25a5208dec
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2026-07-03 20:25:17 +0900

    Create 739.py

---
+++ pending/739.py
@@ -0,0 +1,15 @@
+# 739. Daily Temperatures
+# https://leetcode.com/problems/daily-temperatures/description/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
+        return answer
