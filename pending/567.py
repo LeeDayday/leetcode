7b6c42a6027597cb5d28d66ffb9d6fabaf0a43b9
Lee Dayoon <95182422+LeeDayday@users.noreply.github.com>
2026-07-06 17:46:16 +0900

    Create 567.py

---
+++ pending/567.py
@@ -0,0 +1,22 @@
+# 567. Permutation in String
+# https://leetcode.com/problems/permutation-in-string/
+
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n = len(s1)
+        m = len(s2)
+        s1_counter = Counter(s1)
+        tmp_counter = Counter(s2[:n])
+        if s1_counter == tmp_counter:
+            return True
+
+        for i in range(n, m):
+            left = s2[i - n]
+            right = s2[i]
+            tmp_counter[left] -= 1
+            tmp_counter[right] += 1
+            if s1_counter == tmp_counter:
+                return True
+            
+        return False
